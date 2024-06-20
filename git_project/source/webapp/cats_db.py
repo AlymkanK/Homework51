149968ca6f5ebea273825e4386345f8e2e214670
AlymkanKalykova <alymkan23@gmail.comgit config --global user.name >
2024-06-20 15:57:12 +0600

    Создала gitignore и включила все документы за которыми следить не надо

--- git_project/source/webapp/cats_db.py
+++
@@ -1,2 +0,0 @@
-class CatsDb:
-    cats = []
--- git_project/source/webapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from webapp.views import index, cat_stats, create_cat
-
-urlpatterns = [
-    path('', index),
-    path('cat_stats/', cat_stats),
-    path('create/', create_cat)
-]
